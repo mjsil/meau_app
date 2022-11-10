488df122ea41402bc9425a7b52bf3bcc67b9be78
Maurilio Silveira <maurilio.agile@fleetcor.com.br>
2022-11-10 16:17:38 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Flutter Demo', home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MIAU'),
+      ),
+      body: const Center(
+        child: Text('Seja Bem-Vindo(a)!'),
+      ),
+    );
+  }
+}
