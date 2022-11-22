e6414ed3e737bd5c9d826a75bdcd65036499d152
Maurilio Silveira <maurilio.agile@fleetcor.com.br>
2022-11-22 17:17:43 -0300

    feat: implemented registration system

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -9,67 +17,105 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Flutter Demo', home: LoginScreen());
+    const appTitle = 'Cadastro';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
+      ),
+    );
   }
 }
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  SignUpScreen createState() {
+    return SignUpScreen();
+  }
+}
+
+class SignUpScreen extends State {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.only(
-          top: 64,
-          left: 20,
-          right: 20,
-        ),
-        child: ListView(
-          children: <Widget>[
-            TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                  hintText: "Nome de usuário",
-                  contentPadding: EdgeInsets.all(8),
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
-                  )),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: const InputDecoration(
-                  hintText: "Senha",
-                  contentPadding: EdgeInsets.all(8),
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
-                  )),
-            ),
-            const SizedBox(height: 52),
-            SizedBox(
-              width: 232,
-              height: 40,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  textStyle: const TextStyle(
-                    fontSize: 12,
-                  ),
+    return Form(
+      key: _formKey,
+      child: ListView(
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+                hintText: "Email",
+                contentPadding: EdgeInsets.all(8),
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                )),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            controller: _passwordController,
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            decoration: const InputDecoration(
+                hintText: "Senha",
+                contentPadding: EdgeInsets.all(8),
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                )),
+          ),
+          const SizedBox(height: 52),
+          SizedBox(
+            width: 232,
+            height: 40,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.black,
+                textStyle: const TextStyle(
+                  fontSize: 12,
                 ),
-                onPressed: () => {},
-                child: const Text("ENTRAR"),
               ),
+              onPressed: () => SigUp(),
+              child: const Text("CADASTRAR"),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+
+  void SigUp() async {
+    try {
+      final response =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      if (response != null) {
+        const snackBar = SnackBar(
+          content: Text('Success!'),
+        );
+
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    } catch (error) {
+      const snackBar = SnackBar(
+        content: Text('Error!'),
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 }
