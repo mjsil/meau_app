69c072bdac68b3a6112741bbbb31bd40ff9d28ad
Pedro Henrique Da Costa Vilarins <pedrovilarins077@gmail.com>
2022-12-10 02:53:10 -0300

    fix: final app main function

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Meau',
-      home: SignInScreen()
-    );
+    return const MaterialApp(title: 'Meau', home: SignInScreen());
   }
 }
-
