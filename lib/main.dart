cdbbfe0c6e98687f1bbef78de1a2e1ed45d525c2
Pedro Henrique Da Costa Vilarins <pedrovilarins077@gmail.com>
2022-12-13 05:03:08 -0300

    feat: changed main function to call IntroScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'screens/SplashScreen/index.dart';
 import 'utils/firebase_options.dart';
 import 'package:flutter/material.dart';
 import './screens/SignIn/index.dart';
@@ -17,10 +18,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Meau',
-      home: SignInScreen()
-    );
+    return const MaterialApp(title: 'Meau', home: MyHomePage());
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const IntroScreen();
+  }
+}
