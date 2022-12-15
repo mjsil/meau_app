deb2ab951917856722ddf21544d6881f99cca8b6
Pedro Henrique Da Costa Vilarins <pedrovilarins077@gmail.com>
2022-12-14 22:52:17 -0300

    feat: implemented sidebar menu widget

---
+++ lib/screens/SideBarMenu/index.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:meau_app/screens/AnimalRegistry/index.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  bool _customTileExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: const Text('Pedro'),
+            accountEmail: const Text('pedro@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.asset('assets/ImagemTeste/ImagemDePerfil.jpeg'),
+              ),
+            ),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 136, 201, 191),
+            ),
+          ),
+          ExpansionTile(
+            leading: const Icon(Icons.favorite),
+            title: const Text('Atalhos'),
+            textColor: const Color.fromARGB(255, 67, 67, 67),
+            collapsedBackgroundColor: const Color.fromARGB(255, 254, 226, 155),
+            //backgroundColor: const Color.fromARGB(255, 254, 226, 155),
+            trailing: Icon(
+              _customTileExpanded
+                  ? Icons.arrow_drop_down
+                  : Icons.arrow_drop_down,
+            ),
+            children: <Widget>[
+              const Divider(
+                endIndent: 100,
+              ),
+              ListTile(
+                title: const Text('Cadastrar um pet'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const AnimalRegistryScreen(),
+                    ),
+                  );
+                },
+              ),
+            ],
+            onExpansionChanged: (bool expanded) {
+              setState(() => _customTileExpanded = expanded);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
