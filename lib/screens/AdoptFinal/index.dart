327c684e7525792f8a5a0801dc92b53aabd3a817
Pedro Henrique Da Costa Vilarins <pedrovilarins077@gmail.com>
2022-12-25 17:22:15 -0300

    new AdoptFinalScreen screen added

---
+++ lib/screens/AdoptFinal/index.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AdoptFinalScreen extends StatelessWidget {
+  const AdoptFinalScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Adotar final'),
+      ),
+    );
+  }
+}
