c2beec36f5da41ce87b95fbfb4428cf159e79edb
Pedro Henrique Da Costa Vilarins <pedrovilarins077@gmail.com>
2023-01-03 15:40:42 -0300

    feat: first animalDetail back-end

--- lib/screens/AnimalDetail/index.dart
+++ lib/screens/AnimalDetail/index.dart
@@ -2,16 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class AnimalDetailScreen extends StatelessWidget {
-  const AnimalDetailScreen({super.key});
+  final String name;
+  final String pictureUrl;
+  final String sex;
+  final String size;
+  final String age;
+  final bool castrated;
+  final bool dewormed;
+  final bool vaccinated;
+  final bool sick;
+  //temper
+  final bool playful;
+  final bool shy;
+  final bool calm;
+  final bool watchDog;
+  final bool lovable;
+  final bool lazy;
+  //adoption requirement
+  final bool adoptionTerm;
+  final bool housePicture;
+  final bool previousVisit;
+  //
+  final String sickness;
+  final String history;
+
+  const AnimalDetailScreen({
+    Key? key,
+    required this.name,
+    required this.pictureUrl,
+    required this.sex,
+    required this.size,
+    required this.age,
+    required this.castrated,
+    required this.vaccinated,
+    required this.dewormed,
+    required this.sick,
+    required this.sickness,
+    required this.history,
+    //temper
+    required this.playful,
+    required this.shy,
+    required this.calm,
+    required this.watchDog,
+    required this.lovable,
+    required this.lazy,
+    //adoption requirement
+    required this.adoptionTerm,
+    required this.housePicture,
+    required this.previousVisit,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 250, 250, 250),
       appBar: AppBar(
-        title: const Text(
-          "Bidu",
-          style: TextStyle(
+        title: Text(
+          name,
+          style: const TextStyle(
             fontSize: 20,
             color: Color.fromARGB(255, 67, 67, 67),
             fontFamily: 'Roboto',
@@ -45,10 +93,8 @@ class AnimalDetailScreen extends StatelessWidget {
                   Container(
                     height: 184,
                     decoration: BoxDecoration(
-                      image: const DecorationImage(
-                        image: AssetImage('assets/ImagemTeste/pet.jpeg'),
-                        fit: BoxFit.cover,
-                      ),
+                      image: DecorationImage(
+                          image: NetworkImage(pictureUrl), fit: BoxFit.cover),
                       boxShadow: [
                         BoxShadow(
                           color: Colors.black.withOpacity(0.2),
@@ -80,9 +126,9 @@ class AnimalDetailScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  "Bidu",
-                  style: TextStyle(
+                Text(
+                  name,
+                  style: const TextStyle(
                     fontSize: 16,
                     fontFamily: 'Roboto',
                     fontWeight: FontWeight.w500,
@@ -94,8 +140,8 @@ class AnimalDetailScreen extends StatelessWidget {
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           "SEXO",
                           style: TextStyle(
                             fontSize: 12,
@@ -104,10 +150,10 @@ class AnimalDetailScreen extends StatelessWidget {
                             color: Color.fromARGB(255, 247, 168, 0),
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Text(
-                          "Macho",
-                          style: TextStyle(
+                          sex,
+                          style: const TextStyle(
                             fontSize: 14,
                             fontFamily: 'Roboto',
                             fontWeight: FontWeight.w400,
@@ -119,8 +165,8 @@ class AnimalDetailScreen extends StatelessWidget {
                     const Spacer(),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           "PORTE",
                           style: TextStyle(
                             fontSize: 12,
@@ -129,10 +175,10 @@ class AnimalDetailScreen extends StatelessWidget {
                             color: Color.fromARGB(255, 247, 168, 0),
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Text(
-                          "Médio",
-                          style: TextStyle(
+                          size,
+                          style: const TextStyle(
                             fontSize: 14,
                             fontFamily: 'Roboto',
                             fontWeight: FontWeight.w400,
@@ -144,8 +190,8 @@ class AnimalDetailScreen extends StatelessWidget {
                     const Spacer(),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           "IDADE",
                           style: TextStyle(
                             fontSize: 12,
@@ -154,10 +200,10 @@ class AnimalDetailScreen extends StatelessWidget {
                             color: Color.fromARGB(255, 247, 168, 0),
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Text(
-                          "Adulto",
-                          style: TextStyle(
+                          age,
+                          style: const TextStyle(
                             fontSize: 14,
                             fontFamily: 'Roboto',
                             fontWeight: FontWeight.w400,
@@ -203,8 +249,8 @@ class AnimalDetailScreen extends StatelessWidget {
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           "CASTRADO",
                           style: TextStyle(
                             fontSize: 12,
@@ -213,18 +259,18 @@ class AnimalDetailScreen extends StatelessWidget {
                             color: Color.fromARGB(255, 247, 168, 0),
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Text(
-                          "Não",
-                          style: TextStyle(
+                          castrated ? "`Sim" : "`Não",
+                          style: const TextStyle(
                             fontSize: 14,
                             fontFamily: 'Roboto',
                             fontWeight: FontWeight.w400,
                             color: Color.fromARGB(255, 117, 117, 117),
                           ),
                         ),
-                        SizedBox(height: 16),
-                        Text(
+                        const SizedBox(height: 16),
+                        const Text(
                           "VACINADO",
                           style: TextStyle(
                             fontSize: 12,
@@ -233,10 +279,10 @@ class AnimalDetailScreen extends StatelessWidget {
                             color: Color.fromARGB(255, 247, 168, 0),
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Text(
-                          "Não",
-                          style: TextStyle(
+                          vaccinated ? "`Sim" : "`Não",
+                          style: const TextStyle(
                             fontSize: 14,
                             fontFamily: 'Roboto',
                             fontWeight: FontWeight.w400,
@@ -248,8 +294,8 @@ class AnimalDetailScreen extends StatelessWidget {
                     const Spacer(),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           "VERMIFUGADO",
                           style: TextStyle(
                             fontSize: 12,
@@ -258,18 +304,18 @@ class AnimalDetailScreen extends StatelessWidget {
                             color: Color.fromARGB(255, 247, 168, 0),
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Text(
-                          "Sim",
-                          style: TextStyle(
+                          dewormed ? "`Sim" : "`Não",
+                          style: const TextStyle(
                             fontSize: 14,
                             fontFamily: 'Roboto',
                             fontWeight: FontWeight.w400,
                             color: Color.fromARGB(255, 117, 117, 117),
                           ),
                         ),
-                        SizedBox(height: 16),
-                        Text(
+                        const SizedBox(height: 16),
+                        const Text(
                           "DOENÇAS",
                           style: TextStyle(
                             fontSize: 12,
@@ -278,10 +324,10 @@ class AnimalDetailScreen extends StatelessWidget {
                             color: Color.fromARGB(255, 247, 168, 0),
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Text(
-                          "Nenhuma",
-                          style: TextStyle(
+                          sick ? "`Doente" : "`Nenhuma",
+                          style: const TextStyle(
                             fontSize: 14,
                             fontFamily: 'Roboto',
                             fontWeight: FontWeight.w400,
@@ -298,8 +344,8 @@ class AnimalDetailScreen extends StatelessWidget {
                 const SizedBox(height: 16),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
+                  children: [
+                    const Text(
                       "TEMPERAMENTO",
                       style: TextStyle(
                         fontSize: 12,
@@ -308,10 +354,10 @@ class AnimalDetailScreen extends StatelessWidget {
                         color: Color.fromARGB(255, 247, 168, 0),
                       ),
                     ),
-                    SizedBox(height: 8),
+                    const SizedBox(height: 8),
                     Text(
-                      "Calmo e dócil",
-                      style: TextStyle(
+                      "${playful ? "brincalhão" : ""} ${shy ? "tímido" : ""} ${calm ? "calmo" : ""} ${watchDog ? "guarda" : ""} ${lovable ? "amoroso" : ""} ${lazy ? "preguiçoso" : ""}",
+                      style: const TextStyle(
                         fontSize: 14,
                         fontFamily: 'Roboto',
                         fontWeight: FontWeight.w400,
@@ -325,8 +371,8 @@ class AnimalDetailScreen extends StatelessWidget {
                 const SizedBox(height: 16),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
+                  children: [
+                    const Text(
                       "EXIGÊNCIAS DO DOADOR",
                       style: TextStyle(
                         fontSize: 12,
@@ -335,10 +381,10 @@ class AnimalDetailScreen extends StatelessWidget {
                         color: Color.fromARGB(255, 247, 168, 0),
                       ),
                     ),
-                    SizedBox(height: 8),
+                    const SizedBox(height: 8),
                     Text(
-                      "Termo de adoção, fotos da casa, visita prévia e acompanhamento durante três meses",
-                      style: TextStyle(
+                      "${adoptionTerm ? "termo de adoção" : ""} ${housePicture ? "fotos da casa" : ""} ${previousVisit ? "visita prévia" : ""} ",
+                      style: const TextStyle(
                         fontSize: 14,
                         fontFamily: 'Roboto',
                         fontWeight: FontWeight.w400,
@@ -352,20 +398,20 @@ class AnimalDetailScreen extends StatelessWidget {
                 const SizedBox(height: 16),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
+                  children: [
                     Text(
-                      "MAIS SOBRE BIDU",
-                      style: TextStyle(
+                      "MAIS SOBRE ${name.toUpperCase()}",
+                      style: const TextStyle(
                         fontSize: 12,
                         fontFamily: 'Roboto',
                         fontWeight: FontWeight.w400,
                         color: Color.fromARGB(255, 247, 168, 0),
                       ),
                     ),
-                    SizedBox(height: 8),
+                    const SizedBox(height: 8),
                     Text(
-                      "Bidu é um cão muito dócil e de fácil convivência. Adora caminhadas e se dá muito bem com crianças. Tem muito medo de raios e de chuva, nesses momentos ele requer mais atenção. Está disponível para adoção pois eu e minha família o encontramos na rua e não podemos mantê-lo em nossa casa. ",
-                      style: TextStyle(
+                      history,
+                      style: const TextStyle(
                         fontSize: 14,
                         fontFamily: 'Roboto',
                         fontWeight: FontWeight.w400,
