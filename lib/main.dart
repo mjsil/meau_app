265b066a67193748cedd0ffabe1efd5dc8e9223d
Pedro Henrique Da Costa Vilarins <pedrovilarins077@gmail.com>
2023-01-10 00:39:13 -0300

    feat: implemented notification with firebaseMessaging in main

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,25 @@ import 'package:meau_app/screens/Login/index.dart';
 import 'package:meau_app/services/auth/index.dart';
 import 'package:meau_app/wrapper.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'utils/firebase_options.dart';
 import 'screens/SplashScreen/index.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(const MyApp());
 }
